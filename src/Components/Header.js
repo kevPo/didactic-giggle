8cd2923aef9fc1dff48f49de15a607f34f3c1628
Kevin Poston <kevposton@gmail.com>
2021-05-22 09:09:37 -0400

    adding animation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,26 @@
 import React from "react";
+import Fade from 'react-reveal/Fade';
 
 const Header = () => (
   <header>
-    <div className="intro-box">
-      <img src={ require('../avi-website.png') } />
+    <Fade left>
+      <div className="intro-box">
+        <img src={ require('../avi-website.png') } />
+      </div>
+    </Fade>
+    <Fade right>
+      <div className="bio">
+        <h1>Kevin Poston</h1>
+        <p>
+          Selfless software engineer with ten years of experience developing
+          software for a variety of domains in both the private and public
+          sectors. Specializes in leading teams to build maintainable software
+          with automated testing and well-established design patterns.
+        </p>
+      </div>
+    </Fade>
+    <div>
     </div>
-    <div className="bio">
-      <h1>Kevin Poston</h1>
-      <p>
-        Selfless software engineer with ten years of experience developing
-        software for a variety of domains in both the private and public
-        sectors. Specializes in leading teams to build maintainable software
-        with automated testing and well-established design patterns.
-      </p>
-    </div>
-    <div></div>
   </header>
 );
 
