190ad0cb8e9575bc06a5b1b857103ccfa89b4481
Kevin Poston <kevposton@gmail.com>
2021-06-24 22:30:16 -0400

    Updates

---
+++ precache-manifest.b0cc7eb684cbc53573627d6691819b95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d8817209c20f488734eee30a5dae4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a71940f4bb8c026ece07",
+    "url": "/static/css/main.689a82d2.chunk.css"
+  },
+  {
+    "revision": "495049cfad13e6ff1cb0",
+    "url": "/static/js/2.54f842ca.chunk.js"
+  },
+  {
+    "revision": "90a3291edaca27aa5b2296c0370ffa8a",
+    "url": "/static/js/2.54f842ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a71940f4bb8c026ece07",
+    "url": "/static/js/main.13b45681.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "628fc6874fb03164b4bc50fcd55a2958",
+    "url": "/static/media/avi-website.628fc687.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/didactic-giggle/precache-manifest.e7747b977e4db0d51e658daf8f8304dc.js"
+  "/precache-manifest.b0cc7eb684cbc53573627d6691819b95.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/didactic-giggle/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
